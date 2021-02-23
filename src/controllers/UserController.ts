5e2ee5c982d10658e64dfdbf855b7ecfe4bb4d73
Lucimara Rios Nakata <lurnakata@gmail.com>
2021-02-23 18:11:13 -0300

    NLW4 - segundo dia

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+
+    const usersRepository = getRepository(User);
+
+    const usersAlreadyExist = await usersRepository.findOne({
+      email,
+    });
+
+    if (usersAlreadyExist) {
+      return response.status(400).json({ error: "User already exists!" });
+    }
+
+    const user = usersRepository.create({ name, email });
+
+    await usersRepository.save(user);
+    return response.json(user);
+  }
+}
+
+export { UserController };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  return response.json({ message: "Hello World - NLW4" });
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
 app.listen(3333, () => console.log("Server is running!"));
